61aae231d8a3a287819ac29b1939835a77ab1a8c
Arol Wright <arol@globcal.net>
2018-04-30 18:02:28 -0400

    dubdub3

---
+++ lineage.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2018 The LineageOS Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from falcon device
+$(call inherit-product, device/motorola/falcon/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_falcon
+PRODUCT_DEVICE := dumpling
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Moto G
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="falcon_retuglb-user 5.1 LPB23.13-58 58 release-keys"
+
+BUILD_FINGERPRINT := motorola/falcon_retuglb/falcon_umts:5.1/LPB23.13-58/58:user/release-keys
+
+TARGET_VENDOR := motorola
